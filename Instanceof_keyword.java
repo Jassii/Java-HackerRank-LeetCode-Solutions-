5780eeabdbb2c477f581f34d0138e86f23f0eb4e
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-04-06 20:19:17 +0530

    Create Instanceof_keyword.java

---
+++ Instanceof_keyword.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+
+class Student{}
+class Rockstar{   }
+class Hacker{}
+
+
+public class InstanceOFTutorial{
+	
+   static String count(ArrayList mylist){
+      int a = 0,b = 0,c = 0;
+      for(int i = 0; i < mylist.size(); i++){
+         Object element=mylist.get(i);   //this element is storing the particular value of array list..which is an object...
+         if(element instanceof Student)     //instaceof operator is actually checking whether the object of the particular is same or not..
+            a++;   //if yes it is increasing the count of the same object by 1..
+         if(element instanceof Rockstar)
+            b++;
+         if(element instanceof Hacker)
+            c++;
+      }
+      String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
+      return ret;
+   }
+
+   public static void main(String []args){
+      ArrayList mylist = new ArrayList();  //created an array list...
+      Scanner sc = new Scanner(System.in);
+      int t = sc.nextInt();
+      for(int i=0; i<t; i++){
+         String s=sc.next();
+         if(s.equals("Student"))mylist.add(new Student());  //mylist array list is storing the objects of the respective classes..
+         if(s.equals("Rockstar"))mylist.add(new Rockstar());
+         if(s.equals("Hacker"))mylist.add(new Hacker());
+      }
+      System.out.println(count(mylist));  //this is returning the total count of the objects of the respective classes..
+   }
+}
