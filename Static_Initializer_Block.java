dd2dc000ca4acd3d5cb301981bc11c846c8c8282
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-04-07 21:34:37 +0530

    Create Static_Initializer_Block.java

---
+++ Static_Initializer_Block.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+//Write your code here
+static Scanner sc = new Scanner(System.in);  //assigning this also as static because with the help of the reference variable value is provided to B and H..
+static int B;  //value of breadth..
+static int H;  //value of length..
+
+//alternative method of using static initializer block...  
+public static boolean flag = initializeClassVariable();
+private static boolean initializeClassVariable()
+{
+    B = sc.nextInt();
+    H = sc.nextInt();
+    if(B<=0 || H<=0)   //if any of them is less than zero...
+    {
+        System.out.println("java.lang.Exception: Breadth and height must be positive");
+        System.exit(0);  //when any value is negative then simply terminate the program elseee find the area of the parallelogram..
+    }
+    return true;
+}
+
+public static void main(String[] args)
+{
+		if(flag)  //flag variable is accessed directly as it is static..no need to create an object..
+    {      
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
