1f5f43096ae721a8519a67a0cae16c76bc8ae0f0
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-05-23 19:43:47 +0530

    Create Repeated_String.java

---
+++ Repeated_String.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+
+    public static long repeatedString(String s, long n) 
+    {
+       // Write your code here
+       long size = s.length(); //length of the string which has to be repeated..
+       
+       //how many times it will be repeated..
+       long repeat = n/size;  //(10/3) = 3//(aba aba aba) 
+       
+       //how much is left out..
+       long left = n-(size*repeat);  //(10-(3*3)) = (1)
+       
+       int extra=0;
+       long count=0;
+       int i;
+       
+       //checking the count of 'a' in the string given..
+       for(i=0;i<size;i++)
+       {
+           if(s.charAt(i)=='a')
+           {
+               count++;
+           }
+       }
+        //here count will store 2  (aba)
+        
+       //checking if the extra character is 'a' or not..
+       for(i=0;i<left;i++)
+       {
+           if(s.charAt(i)=='a')
+           {
+               extra++;
+           }
+       } 
+       
+       //now total number of count will be(including the repeation)
+       count = (repeat*count) + extra;
+       return count;
+    }
+}
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = Result.repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
