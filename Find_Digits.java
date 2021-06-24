018e6bd3939bc3c59f46fdaa1687962aeb6f9c7b
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-06-24 23:16:43 +0530

    Create Find_Digits.java

---
+++ Find_Digits.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) 
+    {
+       // Write your code here
+       int r = n;  //save the value of n in r variable.(As we have to check with n again and again..)
+       int count=0;  //count the number of divisors..
+       while(r>0)
+       {
+           if(r%10!=0 && (n%(r%10))==0)  //if the last element is not 0 and is dividing n..
+           {
+               count++;
+           }
+           r = r/10; //decreasing the value of r.
+       }
+       return count;  //return the count..
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
