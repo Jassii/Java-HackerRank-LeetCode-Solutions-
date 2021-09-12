c7af348f08877ca2a146e0e6a8ffdbf569876566
Jasjeet Singh <jasjeetsingh1009@gmail.com>
2021-09-13 01:41:08 +0530

    Create Weather_Observation_Station_13.sql

---
+++ SQL/Weather_Observation_Station_13.sql
@@ -0,0 +1,7 @@
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  and less than . Truncate your answer to 4 decimal places.
+
+Input Format
+
+The STATION table is described as follows:
+
+select round(sum(LAT_N),4) from STATION where LAT_N>38.7880 and LAT_N<137.2345;
